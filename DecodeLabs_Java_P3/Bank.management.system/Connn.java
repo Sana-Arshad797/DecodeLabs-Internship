50576a91d520bcf63c8f9737e43973101fd54800
Sana-Arshad797 <sanaarshad6976@gmail.com>
2026-07-29 12:20:00 +0500

    Add database connection class Connn

---
+++ DecodeLabs_Java_P3/Bank.management.system/Connn.java
@@ -0,0 +1,18 @@
+package Bank.management.system;
+
+import java.sql.*;
+
+public class Connn {
+    Connection connection;
+    Statement statement;
+    public Connn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","Sana1234");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
